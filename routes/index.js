bfc6288b6e96be11aa9e95cd79850aa49aca6684
ettoreciarcia <ettore.ciarcia@epsilonline.com>
2022-08-15 17:09:44 +0200

    Added network info route

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const pki = require('node-forge').pki
+const os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'kubernetes-app' });
 });
 
+/* Crash application */
 router.get('/crash', function (req, res, next) {
   console.log(new Error('Requested crash by endpoint /crash'))
   process.exit(1)
 })
 
+/* CPU intensive fucntion generate CPU spikes */
 router.get('/generatecert', function (req, res, next) {
   const keys = pki.rsa.generateKeyPair(2048)
   const cert = pki.createCertificate()
@@ -19,6 +22,17 @@ router.get('/generatecert', function (req, res, next) {
   res.send({
     keys: keys,
     cert: cert
+  })
+})
+
+/* Get local addredd on which pods/server is running */
+
+router.get('/networkInfo', function(req,res,next){
+  res.send({
+    hostname: os.hostname(),
+    // platform: os.platform(),
+    IP: os.networkInterfaces(),
+    // load: os.loadavg()
   })
 })
 
